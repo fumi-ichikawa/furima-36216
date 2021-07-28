ee3e70f6c0daa823bc379797a3b90ea8c5fdc837
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-07-28 21:47:47 +0900

    出品ページのプルダウンの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+  
+  
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:Item).permit(:title,:description,:category_id,:status_id,:postage_id,:ship_region_id,:shipping_date_id,:price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :ship_region
+  belongs_to :shipping_date
+
+  with_options presence: true do
+    validates :title
+    validates :description
+  end
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :status_id
+    validates :postage_id
+    validates :ship_region_id
+    validates :shipping_date_id
+  end
+
   belongs_to :user
   has_one_attached :image
 end
