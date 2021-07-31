469a2b6a0954177aaef2c1057baeaf79e224a1df
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-07-31 14:31:48 +0900

    メソッドによるコード省略
    「redirect_to action: :index unless @item.user_id == current_user.id」を「move_to_index」として定義しeditとupdateのみに適用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
+
 
   def index
     query = 'SELECT * FROM items order by created_at DESC'
@@ -24,7 +26,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless @item.user_id == current_user.id
   end
 
   def update
@@ -44,6 +45,10 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless @item.user_id == current_user.id
   end
 
 end
