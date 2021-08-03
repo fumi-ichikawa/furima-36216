b5cc0f530dff6fda0f5eebb8cf213339e62a970c
fumi-ichikawa <japanesequeen1114@gmail.com>
2021-08-03 14:46:27 +0900

    shopping_recordモデルのリファクタリング

--- app/models/shopping_profile.rb
+++ app/models/shopping_profile.rb
@@ -4,14 +4,12 @@ class ShoppingProfile
   attr_accessor :postal_code, :ship_region_id, :city, :address, :building_name, :phone, :user_id, :item_id, :token
 
   validates :ship_region_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :postal_code,  format: { with: /\A\d{3}-\d{4}\z/, message: 'はハイフンあり半角数字で入力して下さい' }, allow_blank: true
-  validates :phone, format: { with: /\A\d{10,11}\z/, message: 'はハイフンなし半角数字で入力して下さい' }, allow_blank: true
 
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'はハイフンあり半角数字で入力して下さい' }, allow_blank: true
     validates :city
     validates :address
-    validates :phone
+    validates :phone, format: { with: /\A\d{10,11}\z/, message: 'はハイフンなし半角数字で入力して下さい' }, allow_blank: true
     validates :user_id
     validates :item_id
     validates :token
